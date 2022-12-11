d4df034697ec1452d34fc3b5392bc1595b540315
Olga <olg200919@ya.ru>
2022-12-11 15:15:48 +0300

    Задание 5

--- js/script.js
+++ js/script.js
@@ -15,17 +15,16 @@ document.querySelector('.btn').classList.toggle('btn-secondary');
 document.querySelector('.menu').classList.remove('dropdown-menu');
 */
 
-/*
-4. Используя метод insertAdjacentHTML добавьте после div'a с классом
-"dropdown" следующую разметку:
-  <a href="#">link</a>
- */
+/*4. Используя метод insertAdjacentHTML добавьте после div'a с классом "dropdown" следующую разметку:
+<a href="#">link</a>
+ 
 document.querySelector('div.dropdown').insertAdjacentHTML('afterend', '<a href="#">link</a>');
+*/
 
 /*
 5. У элемента с id "dropdownMenuButton" замените id на "superDropdown".
  */
-
+document.getElementById('dropdownMenuButton').id = 'superDropdown';
 
 /*
 6. Добавьте атрибут data-dd со значением 3 элементу у которого существует
